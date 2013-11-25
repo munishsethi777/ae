5ac19ce14ff2371a1ddb777a8f53dce208bb6e0a
Dalbir Singh <dssohal1@gmail.com>
2013-11-25 15:33:56 +0530

    uploading assessment engine

---
+++ src/validator/AEValidationMessage.java
@@ -0,0 +1,40 @@
+package validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AEValidationMessage {
+	private Object object;
+	public Object getObject() {
+		return object;
+	}
+	public void setObject(Object object) {
+		this.object = object;
+	}
+	private String fieldName;
+	private List<String> errorMessages;
+	
+	public String getFieldName() {
+		return fieldName;
+	}
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+	public List<String> getErrorMessages() {
+		return errorMessages;
+	}
+	public void setErrorMessages(List<String> errorMessages) {
+		this.errorMessages = errorMessages;
+	}
+	
+	public AEValidationMessage(String fieldName){
+		this.setFieldName(fieldName);
+	}
+	public void addErrorMessage(String message){
+		if(this.getErrorMessages() == null){
+			List<String> errMessages = new ArrayList<String>();
+			this.setErrorMessages(errMessages);
+		}
+		this.getErrorMessages().add(message);
+	}
+}
